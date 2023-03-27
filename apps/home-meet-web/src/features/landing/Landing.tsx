77641d44362db4a8bb743a1c2bdd2ed2f155bdb6
hillary azungah <samsonnkrumah253@gmail.com>
2023-03-27 18:25:44 +0530

    reat: landing page responsiveness done

--- apps/home-meet-web/src/features/landing/Landing.tsx
+++ apps/home-meet-web/src/features/landing/Landing.tsx
@@ -1,10 +1,20 @@
 import { NavbarComponent } from '@/components';
+import { Button, Space } from 'antd';
 import React from 'react';
 
 export function Landing(): React.ReactElement {
   return (
-    <div className="bg-white w-[100vw] h-[100vh]">
+    <div className="bg-white w-[100vw] h-[100vh] bg-[url('/svg/amoeba.svg')] bg-no-repeat">
       <NavbarComponent />
+      <div className="flex xs:h-[40vh] sm:h-[50vh] md:h-[90vh] !important h-[90vh] items-center justify-center">
+        <Space direction="vertical">
+          <p className="text-[25px] text-center m-0">Home Meet</p>
+          <p className="text-[15px] text-center">
+            Create and join meetings seemlessly
+          </p>
+          <Button>Create Meeting</Button>
+        </Space>
+      </div>
     </div>
   );
 }
