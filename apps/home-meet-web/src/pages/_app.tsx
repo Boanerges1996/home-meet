11c2bd9b2aebe9afbbe5dae0d9768edb29182c82
hillary azungah <samsonnkrumah253@gmail.com>
2023-03-27 20:46:46 +0530

    feat: axios react-query setup done

--- apps/home-meet-web/src/pages/_app.tsx
+++ apps/home-meet-web/src/pages/_app.tsx
@@ -3,6 +3,18 @@ import '@/styles/globals.css';
 import { ConfigProvider } from 'antd';
 import type { AppProps } from 'next/app';
 
+import { QueryClient, QueryClientProvider } from 'react-query';
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+      retry: false,
+      staleTime: 5 * 60 * 1000,
+    },
+  },
+});
+
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <ConfigProvider
@@ -12,9 +24,11 @@ export default function App({ Component, pageProps }: AppProps) {
         },
       }}
     >
-      <HomeMeetProvider>
-        <Component {...pageProps} />
-      </HomeMeetProvider>
+      <QueryClientProvider client={queryClient}>
+        <HomeMeetProvider>
+          <Component {...pageProps} />
+        </HomeMeetProvider>
+      </QueryClientProvider>
     </ConfigProvider>
   );
 }
---
+++ apps/home-meet-web/src/util/axiosClient.ts
@@ -0,0 +1,67 @@
+'use client';
+
+import axios, { AxiosInstance } from 'axios';
+
+const serverUrl = process.env.NEXT_PUBLIC_SERVER_URL;
+const clientUrl = process.env.NEXT_PUBLIC_CLIENT_URL;
+
+export const axiosClient: AxiosInstance = axios.create({
+  baseURL: serverUrl,
+  headers: {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers':
+      'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  },
+  timeout: 10000,
+});
+
+axiosClient.interceptors.response.use(
+  (response: any) => {
+    return response;
+  },
+  (error: any) => {
+    const res = error?.response;
+
+    if (res?.status === 401) {
+      window.location.href = `${clientUrl}`;
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+axiosClient.interceptors.request.use((config: any) => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
+});
+
+export const requestObject = async function (options: any) {
+  const onSuccess = function (response: any) {
+    // console.debug('Request Successful!', response);
+    return response.data;
+  };
+
+  const onError = function (error: any) {
+    if (error.response) {
+      if (error.response.status === 401) {
+        window.location.href = `${clientUrl}`;
+      }
+    } else {
+    }
+
+    return Promise.reject(error.response || error.message);
+  };
+
+  try {
+    const response = await axiosClient(options);
+    return onSuccess(response);
+  } catch (error) {
+    return onError(error);
+  }
+};
