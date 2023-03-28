f3511f3a6b2728ec7664305767cc12fb72740681
hillary azungah <samsonnkrumah253@gmail.com>
2023-03-28 20:27:40 +0530

    feat(api): socket events added

---
+++ apps/home-meet-server/src/apps/meet-socket/meet-socket.constants.ts
@@ -0,0 +1,4 @@
+export const JOIN_AS_BROADCASTER = 'join-as-broadcaster';
+export const JOIN_AS_VIEWER = 'join-as-viewer';
+export const NEW_VIEWER_JOINED = 'new-viewer-joined';
+export const NEW_OFFER = 'new-offer';
--- apps/home-meet-server/src/apps/meet-socket/meet-socket.gateway.ts
+++ apps/home-meet-server/src/apps/meet-socket/meet-socket.gateway.ts
@@ -6,10 +6,15 @@ import {
   WebSocketServer,
 } from '@nestjs/websockets';
 import { MeetSocketService } from './meet-socket.service';
+import {
+  JOIN_AS_BROADCASTER,
+  JOIN_AS_VIEWER,
+  NEW_OFFER,
+  NEW_VIEWER_JOINED,
+} from './meet-socket.constants';
 
-type RoomAndMembersType = {
-  roomName: string;
-  members: string[];
+type RoomBroadcasters = {
+  [key: string]: string;
 };
 
 @WebSocketGateway({
@@ -19,11 +24,11 @@ type RoomAndMembersType = {
 })
 export class MeetSocketGateway {
   constructor(private readonly meetSocketService: MeetSocketService) {
-    this.rooms = [];
+    this.rooms = {};
   }
 
   private logger: Logger = new Logger(MeetSocketGateway.name);
-  private rooms: RoomAndMembersType[];
+  private rooms: RoomBroadcasters;
 
   @WebSocketServer() server: Server;
 
@@ -32,76 +37,137 @@ export class MeetSocketGateway {
   }
 
   async handleDisconnect(client: Socket): Promise<any> {
-    this.handleLeaveInstanceRoom(client);
     this.logger.log(`Client disconnected: ${client.id}`);
   }
 
   @SubscribeMessage('join-room')
   async handleJoinRoom(client: Socket, room: string): Promise<any> {
     this.logger.log(`Client ${client.id} joined room ${room}`);
-    this.handleJoinInstanceRoom(client, room);
+
     client.join(room);
     console.log(this.server.sockets.adapter.rooms);
   }
 
-  @SubscribeMessage('offer')
+  @SubscribeMessage(JOIN_AS_BROADCASTER)
+  async handleJoinAsBroadcaster(
+    client: Socket,
+    {
+      broadcasterId,
+      roomId,
+    }: {
+      broadcasterId: string;
+      roomId: string;
+    },
+  ): Promise<any> {
+    client.join(roomId);
+    client.join(broadcasterId);
+    this.rooms[roomId] = broadcasterId; // Refactor to make sure only broadcaster can join
+  }
+
+  @SubscribeMessage(JOIN_AS_VIEWER)
+  async handleJoinAsViewer(
+    client: Socket,
+    {
+      roomId,
+      viewerData,
+    }: {
+      roomId: string;
+      viewerData: {
+        name: string;
+        pic: string;
+        viewerId: string;
+        socketId: string;
+      };
+    },
+  ): Promise<any> {
+    client.join(roomId);
+    const broadcasterId = this.rooms[roomId];
+    if (broadcasterId) {
+      this.server.to(broadcasterId).emit(NEW_VIEWER_JOINED, viewerData);
+    }
+  }
+
+  @SubscribeMessage(NEW_OFFER)
   async handleCall(
     client: Socket,
-    { roomId, data }: { roomId: string; data: any },
+    {
+      roomId,
+      data,
+    }: {
+      roomId: string;
+      data: {
+        offer: any;
+        viewerId: string;
+      };
+    },
   ): Promise<any> {
-    this.logger.log(`Client ${client.id} offer for room ${roomId}`);
-    this.server.to(roomId).emit('incoming-offer', data);
-    console.log(this.server.sockets.adapter.rooms);
+    this.logger.log(
+      `Client ${client.id} offered to room ${roomId} to viewer ${data.viewerId}`,
+    );
+    this.server.to(data.viewerId).emit('incoming-offer', data);
+    // console.log(this.server.sockets.adapter.rooms);
   }
 
   @SubscribeMessage('answer')
   async handleAnswer(
     client: Socket,
-    { data, roomId }: { roomId: string; data: any },
+    {
+      data,
+      roomId,
+    }: {
+      roomId: string;
+      data: {
+        answer: any;
+        viewerId: string;
+      };
+    },
   ): Promise<any> {
-    this.logger.log(`Client ${client.id} answered to room ${roomId}`);
-    this.server.to(roomId).emit('incoming-answer', data);
+    const broadcasterId = this.rooms[roomId];
+    if (broadcasterId) {
+      this.logger.log(`Client ${client.id} answered to room ${roomId}`);
+      this.server.to(broadcasterId).emit('incoming-answer', data);
+    }
   }
 
   @SubscribeMessage('exchange-ice-candidates')
   async handleIceCandidate(
     client: Socket,
-    { roomId, data }: { roomId: string; data: any },
+    { roomId, candidate }: { roomId: string; candidate: any },
   ): Promise<any> {
     this.logger.log(`Client ${client.id} ice-candidate ${roomId}`);
-    console.log('ice-candidate', data);
-    this.server.to(roomId).emit('incoming-candidate', data);
+    console.log('ice-candidate', candidate);
+    this.server.to(roomId).emit('incoming-candidate', candidate);
   }
 
-  async handleJoinInstanceRoom(client: Socket, room: string) {
-    const roomExists = !this.rooms.find((r) => r.roomName === room);
-    if (!roomExists) {
-      this.rooms.push({ roomName: room, members: [client.id] });
-      return;
-    }
-    this.rooms = this.rooms.map((r) => {
-      if (r.roomName === room) {
-        r.members.push(client.id);
-      }
-      return r;
-    });
+  // async handleJoinInstanceRoom(client: Socket, room: string) {
+  //   const roomExists = !this.rooms.find((r) => r.roomName === room);
+  //   if (!roomExists) {
+  //     this.rooms.push({ roomName: room, members: [client.id] });
+  //     return;
+  //   }
+  //   this.rooms = this.rooms.map((r) => {
+  //     if (r.roomName === room) {
+  //       r.members.push(client.id);
+  //     }
+  //     return r;
+  //   });
 
-    this.logger.log(`Client ${client.id} joined room`);
+  //   this.logger.log(`Client ${client.id} joined room`);
 
-    console.log(this.server.sockets.adapter.rooms);
-  }
+  //   console.log(this.server.sockets.adapter.rooms);
+  // }
 
-  async handleLeaveInstanceRoom(client: Socket) {
-    const tempRooms: RoomAndMembersType[] = [];
-    this.rooms.forEach((r) => {
-      client.leave(r.roomName);
-      const members = r.members.filter((m) => m !== client.id);
-      if (members.length > 0) {
-        tempRooms.push({ roomName: r.roomName, members });
-      }
-    });
+  // async handleLeaveInstanceRoom(client: Socket) {
+  //   const tempRooms: RoomAndMembersType[] = [];
+  //   this.rooms.forEach((r) => {
+  //     client.leave(r.roomName);
+  //     const members = r.members.filter((m) => m !== client.id);
+  //     if (members.length > 0) {
+  //       tempRooms.push({ roomName: r.roomName, members });
+  //     }
+  //   });
 
-    this.rooms = tempRooms;
-    this.logger.log(`Client ${client.id} left room`);
-  }
+  //   this.rooms = tempRooms;
+  //   this.logger.log(`Client ${client.id} left room`);
+  // }
 }
